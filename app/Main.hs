517250970964243835d9d6a56a9f310931ad3a29
Dirakon <609700zxc@mail.ru>
2022-06-09 16:40:15 +0300

    initial-commit

---
+++ app/Main.hs
@@ -0,0 +1,27 @@
+module Main(main) where
+
+import Graphics.Gloss
+
+width, height, offset :: Int
+width = 300
+height = 300
+offset = 100
+
+window :: Display
+window = InWindow "Game" (width, height) (offset, offset)
+
+background :: Color
+background = black
+
+main :: IO ()
+main = display window background drawing 
+
+drawing :: Picture
+drawing = pictures
+  [ color ballColor $ circleSolid 30
+  , color paddleColor $ rectangleSolid 10 50
+  ]
+  where
+    ballColor = dark red 
+    paddleColor = light (light blue) 
+  
