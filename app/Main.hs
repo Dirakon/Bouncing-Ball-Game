36da72237c9b823ce4fc83c652202774bfe76984
Dirakon <609700zxc@mail.ru>
2022-06-09 20:33:58 +0300

    Cleared code of unneccesary parts

--- app/Main.hs
+++ app/Main.hs
@@ -1,157 +1,88 @@
-module Main(main) where
+module Main (main) where
 
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
 import Graphics.Gloss.Interface.IO.Game
 
+
+type Position = (Float, Float)
+
+type Velocity = (Float, Float)
+
+data PlayerBall = PlayerBall Position Velocity
+
+data EnemyBall = EnemyBall Position
+
+-- | A data structure to hold the state of the game.
+data GameState
+  = Game
+  { 
+    mainBall :: PlayerBall,
+    enemyBalls :: [EnemyBall]
+  }
+
+
 width, height, offset :: Int
-width = 300
-height = 300
+width = 700
+height = 700
 offset = 100
 
-window :: Display
-window = InWindow "Game" (width, height) (offset, offset)
 background :: Color
 background = black
-
 
 -- | Number of frames to show per second.
 fps :: Int
 fps = 60
 
+window :: Display
+window = InWindow "Game" (width, height) (offset, offset)
+
+initialState :: GameState
+initialState =
+  Game
+    { 
+      mainBall = PlayerBall (0, 0) (1, 1),
+      enemyBalls = []
+    }
+
 main :: IO ()
 main = play window background fps initialState render handleKeys update
 
 -- | Update the game by moving the ball and bouncing off walls.
-update ::  Float -> PongGame -> PongGame
-update  seconds = wallBounce . moveBall (seconds*100)
-
-
-wallBounce :: PongGame -> PongGame
-wallBounce game = game { ballVel = (vx, vy') }
-  where
-    -- Radius. Use the same thing as in `render`.
-    radius = 10
-
-    -- The old velocities.
-    (vx, vy) = ballVel game
-
-    vy' = if wallCollision (ballLoc game) radius
-          then
-             -- Update the velocity.
-             -vy
-           else
-            -- Do nothing. Return the old velocity.
-            vy
-
-
-type Radius = Float 
-type Position = (Float, Float)
-
--- | Given position and radius of the ball, return whether a collision occurred.
-wallCollision :: Position -> Radius -> Bool 
-wallCollision (_, y) radius = topCollision || bottomCollision
-  where
-    topCollision    = y - radius <= -fromIntegral width / 2 
-    bottomCollision = y + radius >=  fromIntegral width / 2
-
-
-drawing :: Picture
-drawing = pictures [ball, walls,
-                    mkPaddle rose 120 (-20),
-                    mkPaddle orange (-120) 40]
-  where
-    --  The pong ball.
-    ball = translate (-10) 40 $ color ballColor $ circleSolid 10
-    ballColor = dark red
-
-    --  The bottom and top walls.
-    wall :: Float -> Picture
-    wall offset =
-      translate 0 offset $
-        color wallColor $
-          rectangleSolid 270 10
-
-    wallColor = greyN 0.5
-    walls = pictures [wall 150, wall (-150)]
-
-    --  Make a paddle of a given border and vertical offset.
-    mkPaddle :: Color -> Float -> Float -> Picture
-    mkPaddle col x y = pictures
-      [ translate x y $ color col $ rectangleSolid 26 86
-      , translate x y $ color paddleColor $ rectangleSolid 20 80
-      ]
-
-    paddleColor = light (light blue)
-
--- | A data structure to hold the state of the Pong game.
-data PongGame = Game
-  { ballLoc :: (Float, Float)  -- ^ Pong ball (x, y) location.
-  , ballVel :: (Float, Float)  -- ^ Pong ball (x, y) velocity. 
-  , player1 :: Float           -- ^ Left player paddle height.
-                               -- Zero is the middle of the screen. 
-  , player2 :: Float           -- ^ Right player paddle height.
-  } deriving Show
-
-
-initialState :: PongGame
-initialState = Game
-  { ballLoc = (-10, 30)
-  , ballVel = (1, -3)
-  , player1 = 40
-  , player2 = -80
-  }
-
+update :: Float -> GameState -> GameState
+update seconds = moveBall (seconds * 100)
 
 -- | Convert a game state into a picture.
-render :: PongGame  -- ^ The game state to render.
-       -> Picture   -- ^ A picture of this game state.
-render game =
-  pictures [ball, walls,
-            mkPaddle rose 120 $ player1 game,
-            mkPaddle orange (-120) $ player2 game]
+render ::
+  GameState       -- The game state to render.
+  -> Picture      -- A picture of this game state.
+render state =
+  pictures [ball]
   where
-    --  The pong ball.
-    ball = uncurry translate (ballLoc game) $ color ballColor $ circleSolid 10
+    (PlayerBall playerPosition _) = mainBall state
+    ball = uncurry translate playerPosition $ color ballColor $ circleSolid 10
     ballColor = dark red
 
-    --  The bottom and top walls.
-    wall :: Float -> Picture
-    wall offset =
-      translate 0 offset $
-        color wallColor $
-          rectangleSolid 270 10
-
-    wallColor = greyN 0.5
-    walls = pictures [wall 150, wall (-150)]
-
-    --  Make a paddle of a given border and vertical offset.
-    mkPaddle :: Color -> Float -> Float -> Picture
-    mkPaddle col x y = pictures
-      [ translate x y $ color col $ rectangleSolid 26 86
-      , translate x y $ color paddleColor $ rectangleSolid 20 80
-      ]
-
-    paddleColor = light (light blue)
-  
-
-moveBall seconds game = game { ballLoc = (x', y') }
+moveBall :: Float -> GameState -> GameState
+moveBall seconds state = state {mainBall = movedPlayerBall}
   where
     -- Old locations and velocities.
-    (x, y) = ballLoc game
-    (vx, vy) = ballVel game
+    (PlayerBall playerPos playerVel) = mainBall state
+    (x, y) = playerPos
+    (vx, vy) = playerVel
 
     -- New locations.
     x' = x + vx * seconds
     y' = y + vy * seconds
+    newPos = (x', y')
+    movedPlayerBall = PlayerBall newPos playerVel
 
-
-    -- | Respond to key events.
-handleKeys :: Event -> PongGame -> PongGame
+-- | Respond to key events.
+handleKeys :: Event -> GameState -> GameState
 
 -- For an 's' keypress, reset the ball to the center.
-handleKeys (EventKey (Char 's') _ _ _) game =
-  game { ballLoc = (0, 0) }
-
+handleKeys (EventKey (Char 's') Down _ _) state =
+  state {mainBall = PlayerBall (0, 0) (1, 1)}
+  
 -- Do nothing for all other events.
 handleKeys _ game = game
